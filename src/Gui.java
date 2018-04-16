5a9160946e18b6a38295d9fee6c5f69e3920ea86
JaminROCK <KingJamin@hotmail.com>
2018-04-15 22:57:58 -0600

    movement commit

--- src/Gui.java
+++ src/Gui.java
@@ -16,8 +16,10 @@ import javax.swing.JProgressBar;
 public class Gui extends JFrame implements KeyListener
 {
 	private JPanel panel = new JPanel();
+	private int x = 250, y = 800;
 	public Gui()
 	{
+		super();
 		setTitle("RYU'S QUEST!");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,11 +27,9 @@ public class Gui extends JFrame implements KeyListener
 		setSize(1280, 1024);
 		setLocationRelativeTo(null);
 		
-		
+		this.addKeyListener(this);
 		panel.setLayout(null);
-		
 		add(panel);
-		
 		setVisible(true);
 	}
 	
@@ -53,28 +53,86 @@ public class Gui extends JFrame implements KeyListener
 	
 	public void addImage() throws IOException
 	{
-        String path = "images/Brick01.png";
+        String path = "images/roboNinja01.png";
         File file = new File(path);
         BufferedImage image = ImageIO.read(file);
         JLabel label = new JLabel(new ImageIcon(image));
 		this.panel.add(label);
-		label.setBounds(250, 800,50,50);
+		label.setBounds(x, y,50,50);
 	}
 	
+
 	
-	public void keyPressed(KeyEvent arg0) 
+	public void keyPressed(KeyEvent e) 
+	{
+		if(e.getKeyCode() == KeyEvent.VK_A)
+		{
+			x-=15;
+			try {
+				this.panel.removeAll();
+				this.addImage();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			this.repaint();
+			this.revalidate();
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_D)
+		{
+			x+=15;
+			try {
+				this.panel.removeAll();
+				this.addImage();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			this.repaint();
+			this.revalidate();
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_S)
+		{
+			y+=15;
+			try {
+				this.panel.removeAll();
+				this.addImage();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			this.repaint();
+			this.revalidate();
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_W)
+		{
+			y-=15;
+			try {
+				this.panel.removeAll();
+				this.addImage();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			this.repaint();
+			this.revalidate();
+		}
+		
+		
+		
+	}
+
+
+	public void keyReleased(KeyEvent e) 
 	{
 		
 	}
 
 
-	public void keyReleased(KeyEvent arg0) 
-	{
-		
-	}
-
-
-	public void keyTyped(KeyEvent arg0) 
+	public void keyTyped(KeyEvent e) 
 	{
 		
 	}
